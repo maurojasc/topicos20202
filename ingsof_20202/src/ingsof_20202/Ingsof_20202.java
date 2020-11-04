f1ec6c5171cfcd800b9f7044f787f84acacb5f35
Mauricio <Mauricio@DESKTOP-N1Q25V5>
2020-11-04 14:26:09 -0500

    Modificacion de MAuricio Rojas Versión 3.1 commit versión 3.1 desarrollador1 Version inicial noviembre 4 con dos líneas de bienvenida.

---
+++ ingsof_20202/src/ingsof_20202/Ingsof_20202.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ingsof_20202;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class Ingsof_20202 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("bienvenidos");
+        System.out.println("integrando netbeans con github");
+    }
+    
+}
